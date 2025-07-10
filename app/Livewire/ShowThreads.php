bbbbbe952a8b103e484b43b45efdc2cb4be48dcc
Doniel Acosta <donielacosta1995@gmail.com>
2025-07-10 06:33:28 -0400

    filtrado reactivo

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -7,16 +7,19 @@ use App\Models\Category;
 use App\Models\Thread;
 
 
-class ShowThreads extends Component
-{
+class ShowThreads extends Component{
+    public $search ='';
     public function render(){
 
         $categories = Category::get();
-        $threads    = Thread::latest()->withCount('replies')->get();
+        $threads    = Thread::query();
+        $threads->where('title','like',"%$this->search%");
+        $threads->withCount('replies');
+        $threads->latest();
 
         return view('livewire.show-threads',[
             'categories' => $categories,
-            'threads' => $threads
+            'threads' => $threads->get()
 
         ])->layout('layouts.app');
     }
--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -22,6 +22,14 @@
     </div>
     <div class ="w-full">
             {{-- formulario  --}}
+            <form class ="mb-4">
+                <input 
+                type="text" 
+                placeholder="// ..."
+                class="bg-slate-800 border-0 rounded-md w-1/3 p-3 text-white/60 text-xs"
+                wire:model="search"
+                >
+            </form>
         @foreach ($threads as $thread )
             <div class="rounded-md bg-gradient-to-r from-slate-800 to-slate-900 hover:to-slate-800 mb-4">
                 <div class ="p-4 flex gap-4">
@@ -43,9 +51,9 @@
                                 <span class="text-white/90"> {{ $thread->created_at->diffForHumans() }} </span>
                             </span>
                             <span class="text-slate-700 flex items-center gap-1">
-                            <svg data-slot="icon" class ="h-4" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                             <path clip-rule="evenodd" fill-rule="evenodd" d="M5.337 21.718a6.707 6.707 0 0 1-.533-.074.75.75 0 0 1-.44-1.223 3.73 3.73 0 0 0 .814-1.686c.023-.115-.022-.317-.254-.543C3.274 16.587 2.25 14.41 2.25 12c0-5.03 4.428-9 9.75-9s9.75 3.97 9.75 9c0 5.03-4.428 9-9.75 9-.833 0-1.643-.097-2.417-.279a6.721 6.721 0 0 1-4.246.997Z"></path>
-                            </svg>
+                                <svg data-slot="icon" class ="h-4" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path clip-rule="evenodd" fill-rule="evenodd" d="M5.337 21.718a6.707 6.707 0 0 1-.533-.074.75.75 0 0 1-.44-1.223 3.73 3.73 0 0 0 .814-1.686c.023-.115-.022-.317-.254-.543C3.274 16.587 2.25 14.41 2.25 12c0-5.03 4.428-9 9.75-9s9.75 3.97 9.75 9c0 5.03-4.428 9-9.75 9-.833 0-1.643-.097-2.417-.279a6.721 6.721 0 0 1-4.246.997Z"></path>
+                                </svg>
                                 {{ $thread->replies_count }}
                                  Respuesta{{ $thread->replies_count !== 1 ? 's' : '' }} 
 
