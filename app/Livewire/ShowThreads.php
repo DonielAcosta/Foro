f7375f3bd7b4500433c55e20d043a81d877b5c17
Doniel Acosta <donielacosta1995@gmail.com>
2025-07-09 21:59:12 -0400

    diseno y creacion de preguntas para la vista

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -12,7 +12,7 @@ class ShowThreads extends Component
     public function render(){
 
         $categories = Category::get();
-        $threads    = Thread::get();
+        $threads    = Thread::latest()->withCount('replies')->get();
 
         return view('livewire.show-threads',[
             'categories' => $categories,
