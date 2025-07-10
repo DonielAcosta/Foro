1f5818e229fca2eaa4d9fe2413fae5f6bc6a5633
Doniel Acosta <donielacosta1995@gmail.com>
2025-07-10 17:47:19 -0400

    creacion de respuestas

--- app/Livewire/ShowThread.php
+++ app/Livewire/ShowThread.php
@@ -4,11 +4,28 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Thread;
+use Livewire\Attributes\Validate;
 class ShowThread extends Component
 {
     public Thread $thread;
+    public $body = '';
+
+    public function postReply(){
+        //validate
+        $this->validate(['body' => 'required']);
+
+        //create
+        auth()->user()->replies()->create([
+            'thread_id' => $this->thread->id,
+            'body' => $this->body,
+        ]);
+        //refresh
+        $this->body =" ";
+    }
     public function render()
     {
-        return view('livewire.show-thread');
+        return view('livewire.show-thread',[
+            'replies' => $this->thread->replies()->get(),
+        ]);
     }
 }
