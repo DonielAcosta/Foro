8a9f205031e18bc98920cf37f2c61a051e404ce7
Doniel Acosta <donielacosta1995@gmail.com>
2025-07-11 09:57:39 -0400

    Habilitación de Campos en Formularios creacion de vistas

--- resources/views/thread/form.blade.php
+++ resources/views/thread/form.blade.php
@@ -1,3 +1,30 @@
 <div>
-    Formulario
+    <select 
+    name="category_id"
+    class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs capitalize mb-4"
+    >
+    <option value="">Selecionar Categoria</option>
+        @foreach ($categories as $category )
+            <option 
+            value="{{ $category->id }}"
+            @if($thread->category_id == $category->id)
+                selected
+            @endif
+            >{{ $category->name }}</option>
+        @endforeach
+    </select>
+    <input 
+        type="text"
+        name="title"
+        placeholder="Title"
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
+        value="{{ $thread->title }}"
+    >
+    <textarea
+      name="body"
+      rows="10"
+      placeholder="Descripcion del Problema"
+      class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
+      >{{ $thread->body }}</textarea>
 </div>
+
